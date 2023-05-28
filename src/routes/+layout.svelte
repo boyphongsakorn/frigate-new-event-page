61bddc425b0fba8075f62e1e4538c56cd70093f5
Phongsakorn Wisetthon <boy1556@hotmail.com>
2023-05-29 01:19:24 +0700

    being

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,19 +4,101 @@
 </script>
 
 <div class="app">
-	<Header />
+	<div class="w-full h-16 bg-gray-800 text-white text-2xl flex items-center space-x-4 dark:bg-gray-900 dark:text-white absolute z-40">
+		<img src="http://192.168.31.220:9000/assets/safari-pinned-tab-1264f127.svg" class="h-8 w-8 invert" />
+		<span class="text-2xl font-semibold">FRIGATE</span>
+	</div>
+	<!-- <Header /> -->
 
-	<main>
+	<div class="flex flex-row min-h-screen w-full bg-white dark:bg-gray-900 text-gray-900 dark:text-white z-30">
+		<div class="w-64 h-screen bg-gray-800 text-white flex flex-col space-y-4">
+			<div class="flex items-center space-x-2 p-4">
+				<img src="http://192.168.31.220:9000/assets/safari-pinned-tab-1264f127.svg" class="h-8 w-8 invert" />
+				<span class="text-2xl font-semibold">FRIGATE</span>
+			</div>
+			<!-- a tag from camera menu -->
+			<a href="/camera" class="flex items-center space-x-2 p-4 hover:bg-gray-700">
+				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+						d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+				</svg>
+				<span class="text-xl font-medium">Cameras</span>
+			</a>
+			<!-- a tag from bridseye menu -->
+			<a href="/bridseye" class="flex items-center space-x-2 p-4 hover:bg-gray-700">
+				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+						d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+				</svg>
+				<span class="text-xl font-medium">Bridseye</span>
+			</a>
+			<!-- a tag from events menu -->
+			<a href="/events" class="flex items-center space-x-2 p-4 hover:bg-gray-700">
+				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+						d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+				</svg>
+				<span class="text-xl font-medium">Events</span>
+			</a>
+			<!-- line -->
+			<div class="border-b border-gray-700"></div>
+			<!-- a tag from storage menu -->
+			<a href="/storage" class="flex items-center space-x-2 p-4 hover:bg-gray-700">
+				<!-- storage icon -->
+				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+						d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+				</svg>
+				<span class="text-xl font-medium">Storage</span>
+			</a>
+			<!-- a tag from System menu -->
+			<a href="/system" class="flex items-center space-x-2 p-4 hover:bg-gray-700">
+				<!-- system icon -->
+				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+						d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+				</svg>
+				<span class="text-xl font-medium">System</span>
+			</a>
+			<!-- a tag from Config menu -->
+			<a href="/settings" class="flex items-center space-x-2 p-4 hover:bg-gray-700">
+				<!-- config icon -->
+				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+						d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+				</svg>
+				<span class="text-xl font-medium">Config</span>
+			</a>
+			<!-- a tag from logs menu -->
+			<a href="/logs" class="flex items-center space-x-2 p-4 hover:bg-gray-700">
+				<!-- logs icon -->
+				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+						d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
+				</svg>
+				<span class="text-xl font-medium">Logs</span>
+			</a>
+		</div>
+		<div class="flex flex-col flex-1 pt-16">
+			<slot />
+		</div>
+		<!-- <div class="w-full h-16 bg-gray-800 text-white text-2xl flex items-center space-x-4">
+			<img src="http://192.168.31.220:9000/assets/safari-pinned-tab-1264f127.svg" class="h-8 w-8 invert" />
+			Frigate
+		</div> -->
+	</div>
+
+	<!-- <main>
 		<slot />
-	</main>
+	</main> -->
 
-	<footer>
+	<!-- <footer>
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	</footer> -->
 </div>
 
 <style>
-	.app {
+	/* .app {
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
@@ -31,7 +113,7 @@
 		max-width: 64rem;
 		margin: 0 auto;
 		box-sizing: border-box;
-	}
+	} */
 
 	footer {
 		display: flex;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,93 @@
 	import Counter from './Counter.svelte';
 	import welcome from '$lib/images/svelte-welcome.webp';
 	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import './styles.css';
+
+	// import Plyr from 'plyr';
+
+	// const player = new Plyr('#player');
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
+	<link rel="stylesheet" href="https://cdn.plyr.io/1.8.2/plyr.css">
+	<script src="https://cdn.jsdelivr.net/hls.js/latest/hls.js"></script>
+	<!-- <script src="https://cdn.plyr.io/1.8.2/plyr.js" on:load={() => {
+		var video = document.querySelector('#player');
+
+		if (Hls.isSupported()) {
+			var hls = new Hls();
+			hls.loadSource('https://nvr.pwisetthon.com/vod/event/1685287665.30394-fsaods/master.m3u8');
+			hls.attachMedia(video);
+			hls.on(Hls.Events.MANIFEST_PARSED,function() {
+				video.play();
+			});
+		}
+
+		plyr.setup(video);
+	}}></script> -->
+	<script src="https://cdn.plyr.io/1.8.2/plyr.js"></script>
 </svelte:head>
 
-<section>
+<!-- events title -->
+<h1 class="text-3xl font-bold text-white p-2">EVENTS</h1>
+
+<!-- div camera arrow down after click have popup select camera -->
+<div class="flex flex-row p-2">
+	<div class="flex flex-col bg-gray-500 rounded-md p-2 w-1/5">
+		<!-- Cameras -->
+		<div class="flex justify-between">
+			Cameras
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+			</svg>
+		</div>
+	</div>
+	<div class="flex flex-col bg-gray-500 rounded-md ml-2 p-2 w-1/5">
+		<!-- Labels -->
+		<div class="flex justify-between">
+			Labels
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+			</svg>
+		</div>
+	</div>
+	<div class="flex flex-col bg-gray-500 rounded-md ml-2 p-2 w-1/5">
+		<!-- Zones -->
+		<div class="flex justify-between">
+			Zones
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+			</svg>
+		</div>
+	</div>
+</div>
+
+<!-- <video id="player" playsinline controls>
+	<source src="https://nvr.pwisetthon.com/vod/event/1685287665.30394-fsaods/master.m3u8" type="application/x-mpegURL" />
+</video> -->
+
+<!-- <video id="player" playsinline controls> -->
+	<!-- <source src="https://nvr.pwisetthon.com/vod/event/1685287665.30394-fsaods/master.m3u8" type="application/x-mpegURL" /> -->
+<!-- </video> -->
+
+<!-- <button class="bg-blue-500 rounded-lg" on:click={() => {
+	var video = document.querySelector('#player');
+
+	if (Hls.isSupported()) {
+		var hls = new Hls();
+		hls.loadSource('https://nvr.pwisetthon.com/vod/event/1685287665.30394-fsaods/master.m3u8');
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED,function() {
+			video.play();
+		});
+	}
+
+	plyr.setup(video);
+}}>Play</button> -->
+
+<!-- <section>
 	<h1>
 		<span class="welcome">
 			<picture>
@@ -26,7 +105,7 @@
 	</h2>
 
 	<Counter />
-</section>
+</section> -->
 
 <style>
 	section {
